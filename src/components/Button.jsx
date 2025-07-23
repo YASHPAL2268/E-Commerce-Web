4a36d7b39fa45ec4a3239191164657bd599e5183
Yashpal <jaatyashpal4011@gmail.com>
2025-07-23 23:04:35 +0530

    Initial commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,26 +1,60 @@
+// import axios from 'axios';
+// import React from 'react'
+
+// const Button = ({title, className,product}) => {
+//    const handleAddToCart = async (product) => {
+
+//     const userId = localStorage.getItem("userId");
+
+//     try {
+//       await axios.post("http://localhost:3000/api/cart/add", {
+//         userId,
+//         productId: product._id
+//       })
+//     }
+//     catch (err) {
+// console.log(err , "err")
+//     }
+//   }
+//   return (
+//     <>
+//         <button onClick={()=>handleAddToCart(product)}  className={`btn me-2 ${className}`}>{title}</button>
+//     </>
+//   )
+// }
+
+// export default Button
+
+
+
 import axios from 'axios';
-import React from 'react'
+import React from 'react';
 
-const Button = ({title, className,product}) => {
-   const handleAddToCart = async (product) => {
+const Button = ({ title, className, product }) => {
 
+  const handleAddToCart = async (product) => {
     const userId = localStorage.getItem("userId");
 
     try {
       await axios.post("http://localhost:3000/api/cart/add", {
         userId,
         productId: product._id
-      })
+      });
+
+      // ✅ Small added feature: confirmation message
+      alert(`${product.name || "Item"} added to cart!`);
+    } catch (err) {
+      console.log(err, "err");
     }
-    catch (err) {
-console.log(err , "err")
-    }
-  }
+  };
+
   return (
     <>
-        <button onClick={()=>handleAddToCart(product)}  className={`btn me-2 ${className}`}>{title}</button>
+      <button onClick={() => handleAddToCart(product)} className={`btn me-2 ${className}`}>
+        {title}
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
